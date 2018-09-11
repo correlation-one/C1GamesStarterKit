588f0d6011fb5fc68257f3113c29eef6bd92c4ac
Junaid Nomani <junaid@correlation-one.com>
2018-09-11 19:10:44 -0400

    removed while loop

--- algos/starter-algo/run.ps1
+++ algos/starter-algo/run.ps1
@@ -2,7 +2,3 @@ $scriptPath = Split-Path -parent $PSCommandPath;
 $algoPath = "$scriptPath\algo_strategy.py"
 
 py -3 $algoPath
-
-while ($true) {
-	Start-Sleep -Seconds 5
-}
--- algos/starter-algo/run.sh
+++ algos/starter-algo/run.sh
@@ -2,8 +2,3 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${PYTHON_CMD:-python3} -u "$DIR/algo_strategy.py"
-
-# Keep alive long enough to pass any errors/debug info to replay visualizer
-while true; do
-  	sleep 5
-done
