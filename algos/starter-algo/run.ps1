3c1ce347836aea4ba1ea08fbe509d4dd02dcb232
Ryan McPartlan <ryan@correlation-one.com>
2018-10-05 17:46:13 -0400

    change folder name to zipme

--- algos/starter-algo/run.ps1
+++
@@ -1,4 +0,0 @@
-$scriptPath = Split-Path -parent $PSCommandPath;
-$algoPath = "$scriptPath\algo_strategy.py"
-
-py -3 $algoPath
--- algos/starter-algo/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${PYTHON_CMD:-python3} -u "$DIR/algo_strategy.py"
