1cc656ad6bb9c6fb868f01ef065af2c049af34d2
idraper <isaac@zfamily.org>
2019-04-05 23:20:14 -0700

    modified include files to work with linux as well as CMakeLists

--- community/c++-algo/include/GameLib/src/utilities.h
+++ community/c++-algo/include/GameLib/src/utilities.h
@@ -3,7 +3,7 @@
 
 #include <iostream>
 
-#include "SimpleJSON\src\JSON.h"
+#include "SimpleJSON/src/JSON.h"
 
 namespace terminal {
 
--- community/c++-algo/src/source.cpp
+++ community/c++-algo/src/source.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string>
 
-#include "SimpleJSON\src\JSON.h"
-#include "GameLib\src\utilities.h"
+#include "GameLib/src/utilities.h"
 
 int main(int argc, char * argv[]) {
 
     std::cerr << "Starting C++ Starter Algo" << std::endl;
-    
+
     std::string cmd_str;
 
     for (int i = 0; i < 200; i++) {
 		std::cin >> cmd_str;
         std::cerr << cmd_str << std::endl;
+
+        terminal::Utilities::test();
 
 		std::cout << "[]\n";
 		std::cout << "[]\n";
