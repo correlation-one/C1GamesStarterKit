6c4c7388c2009922d08b83e05ff223ab4c51e893
Isaac <isaac@zfamily.org>
2019-04-08 08:14:56 -0600

    initial creating of GameMap

---
+++ community/c++-algo/include/GameLib/src/gameMap.cpp
@@ -0,0 +1,11 @@
+/*
+Description: Implementations for the algoMap header.
+Last Modified: 08 Apr 2019
+Author: Isaac Draper
+*/
+
+#include "gameMap.h"
+
+namespace terminal {
+    
+}
---
+++ community/c++-algo/include/GameLib/src/gameMap.h
@@ -0,0 +1,28 @@
+/*
+Description: A header to contain a representation of the current game map.
+Last Modified: 08 Apr 2019
+Author: Isaac Draper
+*/
+
+#ifndef ALGO_MAP_H
+#define ALGO_MAP_H
+
+#include "json11/json11.hpp"
+#include "util.h"
+
+namespace terminal {
+
+    using json11::Json;
+
+    /// Holds data about the current game map and provides functions
+    /// useful for getting information related to the map.
+    class GameMap {
+    public:
+
+    private:
+
+    };
+
+}
+
+#endif
