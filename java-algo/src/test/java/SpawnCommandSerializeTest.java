1d08813b7f8d5563e8c2a9fda55ef3bb8422219b
Junaid Nomani <junaid@correlation-one.com>
2020-01-18 02:02:30 -0500

    java algo hotfix

--- java-algo/src/test/java/SpawnCommandSerializeTest.java
+++ java-algo/src/test/java/SpawnCommandSerializeTest.java
@@ -10,121 +10,155 @@ import org.junit.Test;
 public class SpawnCommandSerializeTest {
     @Test public void test() {
         String configData = "{\n" +
-                "  \"debug\": {\n" +
-                "    \"printMapString\": false,\n" +
-                "    \"printTStrings\": false,\n" +
-                "    \"printActStrings\": false,\n" +
-                "    \"printHitStrings\": false,\n" +
-                "    \"printPlayerInputStrings\": false,\n" +
-                "    \"printBotErrors\": false,\n" +
-                "    \"printPlayerGetHitStrings\": false\n" +
+                "  \"seasonCompatibilityModeP1\": 5,\n" +
+                "  \"seasonCompatibilityModeP2\": 5,\n" +
+                "  \"debug\":{\n" +
+                "    \"printMapString\":false,\n" +
+                "    \"printTStrings\":false,\n" +
+                "    \"printActStrings\":false,\n" +
+                "    \"printHitStrings\":false,\n" +
+                "    \"printPlayerInputStrings\":false,\n" +
+                "    \"printBotErrors\":true,\n" +
+                "    \"printPlayerGetHitStrings\":false\n" +
                 "  },\n" +
                 "  \"unitInformation\": [\n" +
                 "    {\n" +
-                "      \"damage\": 0.0,\n" +
-                "      \"cost\": 1.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"display\": \"Filter\",\n" +
-                "      \"range\": 3.0,\n" +
-                "      \"shorthand\": \"FF\",\n" +
-                "      \"stability\": 60.0\n" +
+                "      \"cost1\": 1.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"display\":\"Filter\",\n" +
+                "      \"shorthand\":\"FF\",\n" +
+                "      \"startHealth\":60.0,\n" +
+                "      \"unitCategory\": 0,\n" +
+                "      \"refundPercentage\": 0.75,\n" +
+                "      \"turnsRequiredToRemove\": 1,\n" +
+                "      \"upgrade\": {\n" +
+                "        \"startHealth\": 120.0\n" +
+                "      }\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"damage\": 0.0,\n" +
-                "      \"cost\": 4.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"shieldAmount\": 10.0,\n" +
-                "      \"display\": \"Encryptor\",\n" +
-                "      \"range\": 3.0,\n" +
-                "      \"shorthand\": \"EF\",\n" +
-                "      \"stability\": 30.0\n" +
+                "      \"cost1\":4.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"shieldPerUnit\":3.0,\n" +
+                "      \"display\":\"Encryptor\",\n" +
+                "      \"shieldRange\":3.5,\n" +
+                "      \"shorthand\":\"EF\",\n" +
+                "      \"startHealth\":30.0,\n" +
+                "      \"unitCategory\": 0,\n" +
+                "      \"shieldBonusPerY\": 0.0,\n" +
+                "      \"refundPercentage\": 0.75,\n" +
+                "      \"shieldDecay\": 0.0,\n" +
+                "      \"turnsRequiredToRemove\": 1,\n" +
+                "      \"upgrade\": {\n" +
+                "        \"shieldRange\": 7,\n" +
+                "        \"shieldPerUnit\":4\n" +
+                "      }\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"damage\": 4.0,\n" +
-                "      \"cost\": 3.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"display\": \"Destructor\",\n" +
-                "      \"range\": 3.0,\n" +
-                "      \"shorthand\": \"DF\",\n" +
-                "      \"stability\": 75.0\n" +
+                "      \"attackDamageWalker\":16.0,\n" +
+                "      \"cost1\":6.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"display\":\"Destructor\",\n" +
+                "      \"attackRange\":3.5,\n" +
+                "      \"shorthand\":\"DF\",\n" +
+                "      \"startHealth\":75.0,\n" +
+                "      \"unitCategory\": 0,\n" +
+                "      \"refundPercentage\": 0.75,\n" +
+                "      \"turnsRequiredToRemove\": 1,\n" +
+                "      \"upgrade\": {\n" +
+                "        \"attackDamageWalker\":32.0\n" +
+                "      }\n" +
+                "    },\n" +
+                "\n" +
+                "\n" +
+                "    {\n" +
+                "      \"attackDamageTower\":2.0,\n" +
+                "      \"attackDamageWalker\":2.0,\n" +
+                "      \"playerBreachDamage\":1.0,\n" +
+                "      \"cost2\":1.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"display\":\"Ping\",\n" +
+                "      \"attackRange\":3.5,\n" +
+                "      \"shorthand\":\"PI\",\n" +
+                "      \"startHealth\":15.0,\n" +
+                "      \"speed\":1,\n" +
+                "      \"unitCategory\": 1,\n" +
+                "      \"selfDestructDamageWalker\": 15.0,\n" +
+                "      \"selfDestructDamageTower\": 15.0,\n" +
+                "      \"metalForBreach\": 1.0,\n" +
+                "      \"selfDestructRange\": 1.5,\n" +
+                "      \"selfDestructStepsRequired\": 5\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"damageI\": 1.0,\n" +
-                "      \"damageToPlayer\": 1.0,\n" +
-                "      \"cost\": 1.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"damageF\": 1.0,\n" +
-                "      \"display\": \"Ping\",\n" +
-                "      \"range\": 3.0,\n" +
-                "      \"shorthand\": \"PI\",\n" +
-                "      \"stability\": 15.0,\n" +
-                "      \"speed\": 0.5\n" +
+                "      \"attackDamageWalker\":8.0,\n" +
+                "      \"attackDamageTower\":8.0,\n" +
+                "      \"playerBreachDamage\":1.0,\n" +
+                "      \"cost2\":3.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"display\":\"EMP\",\n" +
+                "      \"attackRange\":4.5,\n" +
+                "      \"shorthand\":\"EI\",\n" +
+                "      \"startHealth\":5.0,\n" +
+                "      \"speed\":0.5,\n" +
+                "      \"unitCategory\": 1,\n" +
+                "      \"selfDestructDamageWalker\": 5.0,\n" +
+                "      \"selfDestructDamageTower\": 5.0,\n" +
+                "      \"metalForBreach\": 1.0,\n" +
+                "      \"selfDestructRange\": 1.5,\n" +
+                "      \"selfDestructStepsRequired\": 5\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"damageI\": 3.0,\n" +
-                "      \"damageToPlayer\": 1.0,\n" +
-                "      \"cost\": 3.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"damageF\": 3.0,\n" +
-                "      \"display\": \"EMP\",\n" +
-                "      \"range\": 5.0,\n" +
-                "      \"shorthand\": \"EI\",\n" +
-                "      \"stability\": 5.0,\n" +
-                "      \"speed\": 0.25\n" +
+                "      \"attackDamageWalker\":20.0,\n" +
+                "      \"playerBreachDamage\":1.0,\n" +
+                "      \"cost2\":1.0,\n" +
+                "      \"getHitRadius\":0.01,\n" +
+                "      \"display\":\"Scrambler\",\n" +
+                "      \"attackRange\":4.5,\n" +
+                "      \"shorthand\":\"SI\",\n" +
+                "      \"startHealth\":40.0,\n" +
+                "      \"speed\":0.25,\n" +
+                "      \"unitCategory\": 1,\n" +
+                "      \"selfDestructDamageWalker\": 40.0,\n" +
+                "      \"selfDestructDamageTower\": 40.0,\n" +
+                "      \"metalForBreach\": 1.0,\n" +
+                "      \"selfDestructRange\": 1.5,\n" +
+                "      \"selfDestructStepsRequired\": 5\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"damageI\": 10.0,\n" +
-                "      \"damageToPlayer\": 1.0,\n" +
-                "      \"cost\": 1.0,\n" +
-                "      \"getHitRadius\": 0.51,\n" +
-                "      \"damageF\": 0.0,\n" +
-                "      \"display\": \"Scrambler\",\n" +
-                "      \"range\": 3.0,\n" +
-                "      \"shorthand\": \"SI\",\n" +
-                "      \"stability\": 40.0,\n" +
-                "      \"speed\": 0.25\n" +
+                "      \"display\":\"Remove\",\n" +
+                "      \"shorthand\":\"RM\"\n" +
                 "    },\n" +
                 "    {\n" +
-                "      \"display\": \"Remove\",\n" +
-                "      \"shorthand\": \"RM\"\n" +
+                "      \"display\":\"Upgrade\",\n" +
+                "      \"shorthand\":\"UP\"\n" +
                 "    }\n" +
                 "  ],\n" +
-                "  \"timingAndReplay\": {\n" +
-                "    \"waitTimeBotMax\": 70000,\n" +
-                "    \"waitTimeManual\": 1820000,\n" +
-                "    \"waitForever\": false,\n" +
-                "    \"waitTimeBotSoft\": 40000,\n" +
-                "    \"replaySave\": 0,\n" +
-                "    \"storeBotTimes\": true\n" +
+                "  \"timingAndReplay\":{\n" +
+                "    \"waitTimeBotMax\":35000,\n" +
+                "    \"playWaitTimeBotMax\":40000,\n" +
+                "    \"waitTimeManual\":1820000,\n" +
+                "    \"waitForever\":false,\n" +
+                "    \"waitTimeBotSoft\":5000,\n" +
+                "    \"playWaitTimeBotSoft\":10000,\n" +
+                "    \"replaySave\":1,\n" +
+                "    \"playReplaySave\":0,\n" +
+                "    \"storeBotTimes\":true,\n" +
+                "    \"waitTimeStartGame\":3000,\n" +
+                "    \"waitTimeEndGame\":3000\n" +
                 "  },\n" +
-                "  \"resources\": {\n" +
-                "    \"turnIntervalForBitCapSchedule\": 10,\n" +
-                "    \"turnIntervalForBitSchedule\": 10,\n" +
-                "    \"bitRampBitCapGrowthRate\": 5.0,\n" +
-                "    \"roundStartBitRamp\": 10,\n" +
-                "    \"bitGrowthRate\": 1.0,\n" +
-                "    \"startingHP\": 30.0,\n" +
-                "    \"maxBits\": 999999.0,\n" +
-                "    \"bitsPerRound\": 5.0,\n" +
-                "    \"coresPerRound\": 4.0,\n" +
-                "    \"coresForPlayerDamage\": 1.0,\n" +
-                "    \"startingBits\": 5.0,\n" +
-                "    \"bitDecayPerRound\": 0.33333,\n" +
-                "    \"startingCores\": 25.0\n" +
-                "  },\n" +
-                "  \"mechanics\": {\n" +
-                "    \"basePlayerHealthDamage\": 1.0,\n" +
-                "    \"damageGrowthBasedOnY\": 0.0,\n" +
-                "    \"bitsCanStackOnDeployment\": true,\n" +
-                "    \"destroyOwnUnitRefund\": 0.5,\n" +
-                "    \"destroyOwnUnitsEnabled\": true,\n" +
-                "    \"stepsRequiredSelfDestruct\": 5,\n" +
-                "    \"selfDestructRadius\": 1.5,\n" +
-                "    \"shieldDecayPerFrame\": 0.15,\n" +
-                "    \"meleeMultiplier\": 0,\n" +
-                "    \"destroyOwnUnitDelay\": 1,\n" +
-                "    \"rerouteMidRound\": true,\n" +
-                "    \"firewallBuildTime\": 0\n" +
+                "  \"resources\":{\n" +
+                "    \"turnIntervalForBitCapSchedule\":10,\n" +
+                "    \"turnIntervalForBitSchedule\":10,\n" +
+                "    \"bitRampBitCapGrowthRate\":5.0,\n" +
+                "    \"roundStartBitRamp\":10,\n" +
+                "    \"bitGrowthRate\":1.0,\n" +
+                "    \"startingHP\":30.0,\n" +
+                "    \"maxBits\":150.0,\n" +
+                "    \"bitsPerRound\":5.0,\n" +
+                "    \"coresPerRound\":5.0,\n" +
+                "    \"coresForPlayerDamage\":1.0,\n" +
+                "    \"startingBits\":5.0,\n" +
+                "    \"bitDecayPerRound\":0.25,\n" +
+                "    \"startingCores\":40.0\n" +
                 "  }\n" +
                 "}\n";
         System.out.println(configData);
