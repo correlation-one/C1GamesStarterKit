ddcb6edd0938a80ee4f5a12a93621e376272441c
Ioannis "Giannis" Pantidis <40605232+AttackingOrDefending@users.noreply.github.com>
2025-03-12 08:51:37 +0100

    Correctly update costs when upgrading

--- python-algo/gamelib/unit.py
+++ python-algo/gamelib/unit.py
@@ -72,7 +72,7 @@ class GameUnit:
         self.max_health = type_config.get("startHealth", self.max_health)
         self.shieldPerUnit = type_config.get("shieldPerUnit", self.shieldPerUnit)
         self.shieldBonusPerY = type_config.get("shieldBonusPerY", self.shieldBonusPerY)
-        self.cost = [type_config.get("cost1", 0) + self.cost[0], type_config.get("cost2", 0) + self.cost[1]]
+        self.cost = [type_config.get("cost1", self.cost[0]) + self.cost[0], type_config.get("cost2", self.cost[1]) + self.cost[1]]
         self.upgraded = True
 
 
