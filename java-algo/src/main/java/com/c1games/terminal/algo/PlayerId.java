996ad729d2b37dc2cd34d4a2787ce14416b0af48
Federico Auster <federico.auster@correlation-one.com>
2026-01-22 12:52:09 -0300

    feat: remove java support

--- java-algo/src/main/java/com/c1games/terminal/algo/PlayerId.java
+++
@@ -1,9 +0,0 @@
-package com.c1games.terminal.algo;
-
-import com.google.gson.annotations.SerializedName;
-
-public enum PlayerId {
-    @SerializedName("0") Error,
-    @SerializedName("1") Player1,
-    @SerializedName("2") Player2,
-}
